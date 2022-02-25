16cd2ed93629eb02c71ca662842ca6b3ea94ecde
Ryan Ahearn <ryan.ahearn@gsa.gov>
2022-02-25 14:43:58 -0500

    Rerun terraform generator

--- terraform/production/main.tf
+++ terraform/production/main.tf
@@ -17,6 +17,18 @@ module "database" {
   rds_plan_name    = "TKTK-production-rds-plan"
 }
 
+module "redis" {
+  source = "../shared/redis"
+
+  cf_user          = var.cf_user
+  cf_password      = var.cf_password
+  cf_org_name      = local.cf_org_name
+  cf_space_name    = local.cf_space_name
+  env              = local.env
+  recursive_delete = local.recursive_delete
+  redis_plan_name  = "TKTK-production-redis-plan"
+}
+
 
 
 ###########################################################################
